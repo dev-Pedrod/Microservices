c8a815633a1c9b28304396a4f21fff2fe852f2f7
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-25 16:55:50 -0300

    project hr-worker created

---
+++ hr-worker/src/test/java/com/devpedrod/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpedrod.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
